492e7a361013652165a4b27fd841cd2c200de7b1
Agustin Amenabar <agustin.amenabar@westwing.de>
2017-01-12 01:59:28 +0100

    Basic cache headers for images

--- src/Core/Controller/CoreController.php
+++ src/Core/Controller/CoreController.php
@@ -31,10 +31,26 @@ class CoreController
         $response = new Response();
         $response->headers->set('Content-Type', 'image/jpeg');
 
+        $expireDate = new \DateTime();
+        $expireDate->add(new \DateInterval('P1Y'));
+        $response->setExpires($expireDate);
+        $longCacheTime = 3600*24*365;
+        $cacheHeaders = [
+            'max_age'       => $longCacheTime,
+            's_maxage'      => $longCacheTime,
+            'public'        => true,
+        ];
+
         if ($image->getOptions()['refresh']) {
+            $cacheHeaders = [];
+            $response->headers->set('Cache-Control', 'no-cache, private');
+            $response->setExpires(null)->expire();
+
             $response->headers->set('im-identify', $image->getImageIdentity());
             $response->headers->set('im-command', $image->getFinalCommandStr());
         }
+
+        $response->setCache($cacheHeaders);
         $response->setContent($imageContent);
 
         $image->unlinkUsedFiles();
