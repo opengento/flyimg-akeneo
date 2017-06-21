95e488ed0bd7a6f073e489ad542decaea972d866
sadok <sadoknet@gmail.com>
2017-06-21 16:07:15 +0200

    Update Benchmark file + Dockerfile

--- benchmark.sh
+++ benchmark.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
-ab -n 1000 -c 5  "http://localhost:8080/upload/w_200,h_200,c_1,rf_1/http://facedetection.jaysalvat.com/img/faces.jpg" > "var/wt_flyimg_rf_1.log"
-ab -n 1000 -c 5  "http://localhost:8080/upload/w_200,h_200,c_1/http://facedetection.jaysalvat.com/img/faces.jpg" > "var/wt_flyimg_rf_0.log"
+
+port=8080
+
+docker run -itd -p $port:80 --name flyimg flyimg/flyimg-build
+
+sleep 2
+
+run() {
+  url="http://localhost:$port/upload/$2/Rovinj-Croatia.jpg"
+  echo "$1 $url"
+
+  echo "GET $url" | vegeta attack \
+    -duration=10s \
+    -rate=50 \ | vegeta report
+  sleep 1
+}
+
+# Run benchmark
+run "Crop" "w_200,h_200,c_1,"
+run "Resize" "w_200,h_200,rz_1"
+run "Rotate" "r_-45,w_400,h_400"
+
+docker rm -f flyimg
