f6ddb30ca1f8741624c6a6509ce30217e0a79b88
sadok <sadoknet@gmail.com>
2017-07-11 07:52:50 +0200

    Add --optimize-autoloader to composer install in case of benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env bash
 
-port=8080
+# check if vegeta installed
+vegeta -version >/dev/null 2>&1 || { echo >&2 "Benchmark require Vegeta but it's not installed. Aborting."; exit 1; }
 
+# which port
+port=8080
+# a random name for the container and the image
 randName=$(cat /dev/random | LC_CTYPE=C tr -dc "[:lower:]" | head -c 8)
 
+# build the image
 docker build -t $randName .
+
+# run the container
 docker run -itd -p $port:80 --name $randName $randName
 
+# sleep 2 sec until the container is ready
 sleep 2
 
-docker exec $randName composer install
+# install php dependencies
+docker exec $randName composer install --no-dev --optimize-autoloader
 
+# run vegeta attack
 run() {
   url="http://localhost:$port/upload/$2/Rovinj-Croatia.jpg"
   echo "$1 $url"
@@ -22,10 +32,11 @@ run() {
   sleep 1
 }
 
-# Run benchmark
+# run benchmark
 run "Crop" "w_200,h_200,c_1"
 run "Resize" "w_200,h_200,rz_1"
 run "Rotate" "r_-45,w_400,h_400"
 
+# remove the container and the image
 docker rm -f $randName
 docker rmi -f $randName
