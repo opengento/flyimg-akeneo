f5cbf7b9845d5270644e7f6d94945f73d7c31eb9
sadok <sadoknet@gmail.com>
2017-07-30 11:42:44 +0200

    Fix phpcs warining, closes #110

--- tests/Core/Processor/ExtractProcessorTest.php
+++ tests/Core/Processor/ExtractProcessorTest.php
@@ -13,7 +13,10 @@ class ExtractProcessorTest extends BaseTest
 
     public function testExecuteSuccess()
     {
-        $image = $this->ImageHandler->processImage('e_1,p1x_100,p1y_100,p2x_300,p2y_300,o_jpg,rf_1', parent::EXTRACT_TEST_IMAGE);
+        $image = $this->ImageHandler->processImage(
+            'e_1,p1x_100,p1y_100,p2x_300,p2y_300,o_jpg,rf_1',
+            parent::EXTRACT_TEST_IMAGE
+        );
         $image1 = new \Imagick($image->getOutputImagePath());
         $image2 = new \Imagick(parent::EXTRACT_TEST_IMAGE_RESULT);
         $result = $image1->compareImages($image2, \Imagick::METRIC_MEANSQUAREERROR);
