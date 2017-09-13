725947c246e9b208f82035931eb9d002cb7ee0ee
Agustin Amenabar <agustin.amenabar@westwing.de>
2017-09-13 23:46:06 +0200

    Updated and added tests cases for expanding images without stretching. Added cases for partial croping to the sides or top and bottom.

--- tests/Core/Processor/ImageProcessorTest.php
+++ tests/Core/Processor/ImageProcessorTest.php
@@ -36,7 +36,7 @@ class ImageProcessorTest extends BaseTest
      */
     public function testShrinkSuccess(string $options, string $expectedSize, string $sourceImage)
     {
-        $image = $this->ImageHandler->processImage($options . ',o_png', $sourceImage);
+        $image = $this->ImageHandler->processImage($options, $sourceImage);
         $this->generatedImage[] = $image;
         $this->assertFileExists($image->getOutputImagePath());
         $imageDimensions = $this->getImageInfo($image->getOutputImagePath())[ImageMetaInfo::IMAGE_PROP_DIMENSIONS];
@@ -48,7 +48,7 @@ class ImageProcessorTest extends BaseTest
      */
     public function testExpandSuccess(string $options, string $expectedSize, string $sourceImage)
     {
-        $image = $this->ImageHandler->processImage($options . ',o_png', $sourceImage);
+        $image = $this->ImageHandler->processImage($options, $sourceImage);
         $this->generatedImage[] = $image;
         $this->assertFileExists($image->getOutputImagePath());
         $imageDimensions = $this->getImageInfo($image->getOutputImagePath())['dimensions'];
@@ -58,7 +58,8 @@ class ImageProcessorTest extends BaseTest
     public function shrinkProvider(): array
     {
         $resizingTests = [
-        /*   Test name                      url option, out size, source image */
+        /*   Test name  =>
+            url option, out size, source image */
             'Resize to width square' =>
             ['w_300', '300x300', self::PNG_TEST_SQUARE_IMAGE],
             'Resize to width landscape' =>
@@ -125,61 +126,105 @@ class ImageProcessorTest extends BaseTest
     public function expandProvider(): array
     {
         $resizingTests = [
-        /*   Test name                      url option, out size, source image */
-            'Resize to width square' =>
+        /*   Test name   =>
+             url option, out size, source image */
+            'Expand to width square' =>
             ['w_400', '200x200', self::PNG_TEST_SMALL_SQUARE_IMAGE],
-            'Resize to width landscape' =>
+            'Expand to width landscape' =>
             ['w_400', '300x200', self::PNG_TEST_SMALL_LANDSCAPE_IMAGE],
-            'Resize to width portrait' =>
+            'Expand to width portrait' =>
             ['w_400', '200x300', self::PNG_TEST_SMALL_PORTRAIT_IMAGE],
-            'Resize to height square' =>
+
+            'Expand to height square' =>
             ['h_400', '200x200', self::PNG_TEST_SMALL_SQUARE_IMAGE],
-            'Resize to height landscape' =>
+            'Expand to height landscape' =>
             ['h_400', '300x200', self::PNG_TEST_SMALL_LANDSCAPE_IMAGE],
-            'Resize to height portrait' =>
+            'Expand to height portrait' =>
             ['h_400', '200x300', self::PNG_TEST_SMALL_PORTRAIT_IMAGE],
-            'Resize to width and height (landscape) square' =>
-            ['w_400,h_150', '150x150', self::PNG_TEST_SMALL_SQUARE_IMAGE],
-            'Resize to width and height (landscape) landscape' =>
-            ['w_400,h_150', '225x150', self::PNG_TEST_SMALL_LANDSCAPE_IMAGE],
-            'Resize to width and height (landscape) portrait' =>
-            ['w_400,h_150', '100x150', self::PNG_TEST_SMALL_PORTRAIT_IMAGE],
-            'Resize to width and height (portrait) square' =>
-            ['w_150,h_400', '150x150', self::PNG_TEST_SMALL_SQUARE_IMAGE],
-            'Resize to width and height (portrait) landscape' =>
-            ['w_150,h_400', '150x100', self::PNG_TEST_SMALL_LANDSCAPE_IMAGE],
-            'Resize to width and height (portrait) portrait' =>
-            ['w_150,h_400', '150x225', self::PNG_TEST_SMALL_PORTRAIT_IMAGE],
-            'Resize and Crop to square a square' =>
+
+            'Expand to width and height (landscape) square' =>
+            ['w_400,h_300', '200x200', self::PNG_TEST_SMALL_SQUARE_IMAGE],
+            'Expand to width and height (landscape) landscape' =>
+            ['w_400,h_300', '300x200', self::PNG_TEST_SMALL_LANDSCAPE_IMAGE],
+            'Expand to width and height (landscape) portrait' =>
+            ['w_400,h_350', '200x300', self::PNG_TEST_SMALL_PORTRAIT_IMAGE],
+
+            'Expand to width and height (portrait) square' =>
+            ['w_320,h_400', '200x200', self::PNG_TEST_SMALL_SQUARE_IMAGE],
+            'Expand to width and height (portrait) landscape' =>
+            ['w_320,h_400', '300x200', self::PNG_TEST_SMALL_LANDSCAPE_IMAGE],
+            'Expand to width and height (portrait) portrait' =>
+            ['w_320,h_400', '200x300', self::PNG_TEST_SMALL_PORTRAIT_IMAGE],
+
+            'Expand and Crop to square a square' =>
             ['w_400,h_400,c_1', '200x200', self::PNG_TEST_SMALL_SQUARE_IMAGE],
-            'Resize and Crop to square a landscape' =>
-            ['w_400,h_400,c_1', '200x200', self::PNG_TEST_SMALL_LANDSCAPE_IMAGE],
-            'Resize and Crop to square a portrait' =>
-            ['w_400,h_400,c_1', '200x200', self::PNG_TEST_SMALL_PORTRAIT_IMAGE],
-            'Resize and Crop to portrait (wider than portrait) square' =>
-            ['w_250,h_400,c_1', '250x300', self::PNG_TEST_SMALL_SQUARE_IMAGE],
-            'Resize and Crop to portrait (wider than portrait) landscape' =>
-            ['w_250,h_400,c_1', '250x300', self::PNG_TEST_SMALL_LANDSCAPE_IMAGE],
-            'Resize and Crop to portrait (wider than portrait) portrait' =>
-            ['w_250,h_400,c_1', '250x300', self::PNG_TEST_SMALL_PORTRAIT_IMAGE],
-            'Resize and Crop to portrait (narrower than portrait) square' =>
-            ['w_150,h_400,c_1', '150x300', self::PNG_TEST_SMALL_SQUARE_IMAGE],
-            'Resize and Crop to portrait (narrower than portrait) landscape' =>
-            ['w_150,h_400,c_1', '150x300', self::PNG_TEST_SMALL_LANDSCAPE_IMAGE],
-            'Resize and Crop to portrait (narrower than portrait) portrait' =>
-            ['w_150,h_400,c_1', '150x300', self::PNG_TEST_SMALL_PORTRAIT_IMAGE],
-            'Resize and Crop to landscape (taller than landscape) square' =>
-            ['w_400,h_250,c_1', '300x250', self::PNG_TEST_SMALL_SQUARE_IMAGE],
-            'Resize and Crop to landscape (taller than landscape) landscape' =>
-            ['w_400,h_250,c_1', '300x250', self::PNG_TEST_SMALL_LANDSCAPE_IMAGE],
-            'Resize and Crop to landscape (taller than landscape) portrait' =>
-            ['w_400,h_250,c_1', '300x250', self::PNG_TEST_SMALL_PORTRAIT_IMAGE],
-            'Resize and Crop to landscape (shorter than landscape) square' =>
-            ['w_400,h_150,c_1', '300x150', self::PNG_TEST_SMALL_SQUARE_IMAGE],
-            'Resize and Crop to landscape (shorter than landscape) landscape' =>
-            ['w_400,h_150,c_1', '300x150', self::PNG_TEST_SMALL_LANDSCAPE_IMAGE],
-            'Resize and Crop to landscape (shorter than landscape) portrait' =>
-            ['w_400,h_150,c_1', '300x150', self::PNG_TEST_SMALL_PORTRAIT_IMAGE],
+            'Expand and Crop to square a landscape' =>
+            ['w_400,h_400,c_1', '300x200', self::PNG_TEST_SMALL_LANDSCAPE_IMAGE],
+            'Expand and Crop to square a portrait' =>
+            ['w_400,h_400,c_1', '200x300', self::PNG_TEST_SMALL_PORTRAIT_IMAGE],
+
+            'Expand and Crop to portrait (wider a.r. than portrait) square' =>
+            ['w_310,h_600,c_1', '200x200', self::PNG_TEST_SMALL_SQUARE_IMAGE],
+            'Expand and Crop to portrait (wider a.r. than portrait) landscape' =>
+            ['w_310,h_600,c_1', '300x200', self::PNG_TEST_SMALL_LANDSCAPE_IMAGE],
+            'Expand and Crop to portrait (wider a.r. than portrait) portrait' =>
+            ['w_310,h_600,c_1', '200x300', self::PNG_TEST_SMALL_PORTRAIT_IMAGE],
+
+            'Expand and Crop to portrait (narrower a.r. than portrait) square' =>
+            ['w_320,h_640,c_1', '200x200', self::PNG_TEST_SMALL_SQUARE_IMAGE],
+            'Expand and Crop to portrait (narrower a.r. than portrait) landscape' =>
+            ['w_320,h_640,c_1', '300x200', self::PNG_TEST_SMALL_LANDSCAPE_IMAGE],
+            'Expand and Crop to portrait (narrower a.r. than portrait) portrait' =>
+            ['w_320,h_400,c_1', '200x300', self::PNG_TEST_SMALL_PORTRAIT_IMAGE],
+
+            'Expand and Crop to landscape (taller a.r. than landscape) square' =>
+            ['w_380,h_320,c_1', '200x200', self::PNG_TEST_SMALL_SQUARE_IMAGE],
+            'Expand and Crop to landscape (taller a.r. than landscape) landscape' =>
+            ['w_380,h_320,c_1', '300x200', self::PNG_TEST_SMALL_LANDSCAPE_IMAGE],
+            'Expand and Crop to landscape (taller a.r. than landscape) portrait' =>
+            ['w_380,h_320,c_1', '200x300', self::PNG_TEST_SMALL_PORTRAIT_IMAGE],
+
+            'Expand and Crop to landscape (shorter a.r. than landscape) square' =>
+            ['w_600,h_300,c_1', '200x200', self::PNG_TEST_SMALL_SQUARE_IMAGE],
+            'Expand and Crop to landscape (shorter a.r. than landscape) landscape' =>
+            ['w_600,h_300,c_1', '300x200', self::PNG_TEST_SMALL_LANDSCAPE_IMAGE],
+            'Expand and Crop to landscape (shorter a.r. than landscape) portrait' =>
+            ['w_600,h_300,c_1', '200x300', self::PNG_TEST_SMALL_PORTRAIT_IMAGE],
+
+            // test partial crops without expanding
+            'Expand and partial crop to square a landscape' =>
+            ['w_250,h_250,c_1', '250x200', self::PNG_TEST_SMALL_LANDSCAPE_IMAGE],
+            'Expand and partial crop to square a portrait' =>
+            ['w_250,h_250,c_1', '200x250', self::PNG_TEST_SMALL_PORTRAIT_IMAGE],
+
+            'Expand and partial crop to portrait (wider a.r. than portrait) square' =>
+            ['w_190,h_220,c_1', '190x200', self::PNG_TEST_SMALL_SQUARE_IMAGE],
+            'Expand and partial crop to portrait (wider a.r. than portrait) landscape' =>
+            ['w_210,h_300,c_1', '210x200', self::PNG_TEST_SMALL_LANDSCAPE_IMAGE],
+            'Expand and partial crop to portrait (wider a.r. than portrait) portrait' =>
+            ['w_210,h_290,c_1', '200x290', self::PNG_TEST_SMALL_PORTRAIT_IMAGE],
+
+            'Expand and partial crop to portrait (narrower a.r. than portrait) square' =>
+            ['w_190,h_300,c_1', '190x200', self::PNG_TEST_SMALL_SQUARE_IMAGE],
+            'Expand and partial crop to portrait (narrower a.r. than portrait) landscape' =>
+            ['w_190,h_350,c_1', '190x200', self::PNG_TEST_SMALL_LANDSCAPE_IMAGE],
+            'Expand and partial crop to portrait (narrower a.r. than portrait) portrait' =>
+            ['w_190,h_350,c_1', '190x300', self::PNG_TEST_SMALL_PORTRAIT_IMAGE],
+
+            'Expand and partial crop to landscape (taller a.r. than landscape) square' =>
+            ['w_250,h_190,c_1', '200x190', self::PNG_TEST_SMALL_SQUARE_IMAGE],
+            'Expand and partial crop to landscape (taller a.r. than landscape) landscape' =>
+            ['w_290,h_210,c_1', '290x200', self::PNG_TEST_SMALL_LANDSCAPE_IMAGE],
+            'Expand and partial crop to landscape (taller a.r. than landscape) portrait' =>
+            ['w_290,h_210,c_1', '200x210', self::PNG_TEST_SMALL_PORTRAIT_IMAGE],
+
+            'Expand and partial crop to landscape (shorter a.r. than landscape) square' =>
+            ['w_320,h_190,c_1', '200x190', self::PNG_TEST_SMALL_SQUARE_IMAGE],
+            'Expand and partial crop to landscape (shorter a.r. than landscape) landscape' =>
+            ['w_320,h_190,c_1', '300x190', self::PNG_TEST_SMALL_LANDSCAPE_IMAGE],
+            'Expand and partial crop to landscape (shorter a.r. than landscape) portrait' =>
+            ['w_320,h_190,c_1', '200x190', self::PNG_TEST_SMALL_PORTRAIT_IMAGE],
+
         ];
 
         return $this->addOutputExtensionsToTests($resizingTests);
@@ -191,8 +236,7 @@ class ImageProcessorTest extends BaseTest
 
         foreach ($transformationsList as $key => $test) {
             foreach (self::OUTPUT_EXTENSIONS as $extension) {
-                $test[0] = $test[0].',o_'.$extension;
-                $tests[$key . ' with ' . $extension] = $test;
+                $tests[$key . ' with ' . $extension] = [$test[0].',o_'.$extension, $test[1], $test[2]];
             }
         }
 
