c5cb38583373724ba4fc0dfd85a67afe6f6414ea
sadok <sadoknet@gmail.com>
2017-10-25 21:52:06 +0200

    [issue-#101] Add Missing Headers, closes#101

--- src/Core/Controller/CoreController.php
+++ src/Core/Controller/CoreController.php
@@ -108,6 +108,12 @@ class CoreController
             $response->headers->set('im-identify', $imageHandler->imageProcessor()->imageIdentityInformation($image));
             $response->headers->set('im-command', $image->getCommandString());
         }
+        $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+        $response->headers->set('Content-Security-Policy', "script-src 'self'");
+        $response->headers->set('X-Frame-Options', 'SAMEORIGIN');
+        $response->headers->set('X-XSS-Protection', '1; mode=block');
+        $response->headers->set('X-Content-Type-Options', 'nosniff');
+        $response->headers->set('Referrer-Policy', 'origin');
 
         return $response;
     }
